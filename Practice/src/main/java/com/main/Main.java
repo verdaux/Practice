9cb4e7dd678e49f1cb24a49a82e6eb70f5f2b8f7
verdaux <xachineon@gmail.com>
2022-03-14 12:53:14 +0530

    array duplicates added.

--- Practice/src/main/java/com/main/Main.java
+++ Practice/src/main/java/com/main/Main.java
@@ -1,9 +1,14 @@
 package com.main;
 
+import com.arrays.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("calling..."); 
+	public static void main(String[] args)
+	{
+		System.out.println("calling...");
+		ArraysOperations dup_finder = new ArraysOperations();
+		dup_finder.findDuplicates();
 	}
 
 }
